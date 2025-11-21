55f5218b3a23c2675314bd13ebc670273b778bc1
ujwaldeepkadiyam <ujwal.gsites@gmail.com>
2025-11-21 11:03:09 +0000

    Deploying to gh-pages from @ ujwaldeepkadiyam/ujwaldeepkadiyam.github.io@c2987451184890feff22dccc5c7a22ea711e0e27 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -94,17 +94,6 @@ ninja.data = [{
             window.location.href = "/blog/2024/photo-gallery/";
           
         },
-      },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
-        
-          title: 'Google Gemini updates: Flash 1.5, Gemma 2 and Project Astra <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-        
-        description: "We’re sharing updates across our Gemini family of models and a glimpse of Project Astra, our vision for the future of AI assistants.",
-        section: "Posts",
-        handler: () => {
-          
-            window.open("https://blog.google/technology/ai/google-gemini-update-flash-ai-assistant-io-2024/", "_blank");
-          
-        },
       },{id: "post-a-post-with-tabs",
         
           title: "a post with tabs",
@@ -325,17 +314,6 @@ ninja.data = [{
             window.location.href = "/blog/2022/giscus-comments/";
           
         },
-      },{id: "post-displaying-external-posts-on-your-al-folio-blog",
-        
-          title: 'Displaying External Posts on Your al-folio Blog <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-        
-        description: "",
-        section: "Posts",
-        handler: () => {
-          
-            window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
-          
-        },
       },{id: "post-a-post-with-redirect",
         
           title: "a post with redirect",
@@ -546,6 +524,21 @@ ninja.data = [{
           description: "",
           section: "Teaching",handler: () => {
               window.location.href = "/teaching/Data%20Science/Python-for-Data-Wrangling/quick_reference/lec_6/";
+            },},{id: "teaching-unit-1-introduction-and-regression",
+          title: 'Unit-1 Introduction and Regression',
+          description: "",
+          section: "Teaching",handler: () => {
+              window.location.href = "/teaching/Foundations-of-Computing/Foundations-of-Machine-Learning/ML-Unit-1/";
+            },},{id: "teaching-foundations-of-machine-learning",
+          title: 'Foundations of Machine-Learning',
+          description: "",
+          section: "Teaching",handler: () => {
+              window.location.href = "/teaching/Foundations%20of%20Computing/Foundations-of-Machine-Learning/";
+            },},{id: "teaching-foundations-of-computing",
+          title: 'Foundations of Computing',
+          description: "",
+          section: "Teaching",handler: () => {
+              window.location.href = "/teaching/Foundations-of-Computing/";
             },},{id: "teaching-mathematical-foundations",
           title: 'Mathematical Foundations',
           description: "",
