8577d7b659cfeae334f2e6a7047b21444e5b0960
ujwaldeepkadiyam <ujwal.gsites@gmail.com>
2025-11-21 12:17:58 +0000

    Deploying to gh-pages from @ ujwaldeepkadiyam/ujwaldeepkadiyam.github.io@b4bf36fd8b576f64e5c503ee79ccf01b69a95161 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-foundations-of-machine-learning",
-          title: "Foundations of Machine-Learning",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/_teaching/Foundations%20of%20Computing/Foundations-of-Machine-Learning/";
-          },
-        },{id: "nav-blog",
+  },{id: "nav-blog",
           title: "blog",
           description: "",
           section: "Navigation",
