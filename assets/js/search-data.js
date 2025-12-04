6d123a7b1750c0a78a0673ac770693c0a184f038
ujwaldeepkadiyam <ujwal.gsites@gmail.com>
2025-12-04 10:24:32 +0000

    Deploying to gh-pages from @ ujwaldeepkadiyam/ujwaldeepkadiyam.github.io@4f50a20ef23d83f58bf58edc1f356ede57f70f60 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -574,6 +574,11 @@ ninja.data = [{
           description: "",
           section: "Teaching",handler: () => {
               window.location.href = "/teaching/Mathematical%20Foundations/Mathematics-for-computer-science/MFCS-UNIT-1/";
+            },},{id: "teaching-unit-1-mathematical-logic-and-predicate-calculus",
+          title: 'Unit-1 Mathematical Logic and Predicate Calculus',
+          description: "",
+          section: "Teaching",handler: () => {
+              window.location.href = "/teaching/Mathematical%20Foundations/Mathematics-for-computer-science/MFCS-UNIT-1/";
             },},{id: "teaching-unit-2a-set-theory-and-relations",
           title: 'Unit-2A Set Theory And Relations',
           description: "",
